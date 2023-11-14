fd4c4a09f8ef0df0b41ed2a877f1dd525a359b3e
AlsoSylv <64978191+AlsoSylv@users.noreply.github.com>
2023-11-14 13:30:01 -0500

    Serialize for error

--- launcher_core/src/lib.rs
+++ launcher_core/src/lib.rs
@@ -55,6 +55,15 @@ impl ToString for Error {
     }
 }
 
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_str())
+    }
+}
+
 impl AsyncLauncher {
     pub fn new(client: reqwest::Client) -> Self {
         Self { client }
